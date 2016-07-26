5e78df4dbbf6105cc587b62d8dae84c3bb3832c0
Anton Akilov <Anton Akilov>
2016-07-26 20:16:15 +0300

    new block info

---
+++ js/hide.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	  $('.dropdown-menu').hide();
+	  if( $('.win-icon').click & $('.dropdown-menu').hide())
+	   $('.dropdowm-menu').show();	   
+	  else 	$('.dropdowm-menu').hide();	   }
+};
